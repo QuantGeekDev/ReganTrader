530c03aa2b84e8fe6dfbd1b795616e10bc3e5b1f
OnlinePirateDogFromSpaceAndBeyond2020 <58406316+OnlinePirateDogFromSpaceAndBeyond2020@users.noreply.github.com>
2023-08-01 15:59:07 +0200

    Dev0.2

--- backtesting_engine/backtesting_engine.py
+++ backtesting_engine/backtesting_engine.py
@@ -1,11 +1,18 @@
 import backtrader as bt
 import datetime
 import yfinance as yf
+import pandas
 
+cerebro = bt.Cerebro()
 
+df = yf.download('AAPL', start='2010-01-01')
 
+feed = bt.feeds.PandasData(dataname=df)
 
+cerebro.adddata(feed)
 
+cerebro.run()
+cerebro.plot()
 class BacktestingEngine:
     def __init__(self):
         pass
