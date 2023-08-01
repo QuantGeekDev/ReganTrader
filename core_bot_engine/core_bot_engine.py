668e81ecef608a9b7961e80863e0af14ac9c3d41
OnlinePirateDogFromSpaceAndBeyond2020 <58406316+OnlinePirateDogFromSpaceAndBeyond2020@users.noreply.github.com>
2023-08-01 16:28:32 +0200

    Base Version of ReganTrader Dev 0.1

--- core_bot_engine/core_bot_engine.py
+++ core_bot_engine/core_bot_engine.py
@@ -1,34 +1,54 @@
+import time
+from database_manager.database_manager import DatabaseManager
+from data_provider_interface.data_provider_interface import DataProviderInterface
+from strategy_manager.strategy_manager import StrategyManager
+from trade_manager.trade_manager import TradeManager
+
 class CoreBotEngine:
-    def __init__(self, strategy_manager, order_execution_engine, data_provider_interface, trade_manager):
-        self.running = False
-        self.strategy_manager = strategy_manager
-        self.order_execution_engine = order_execution_engine
-        self.data_provider_interface = data_provider_interface
-        self.trade_manager = trade_manager
+    def __init__(self):
+        # Initialize the Database Management and get the last saved settings
+        self.db_manager = DatabaseManager()
+        self.settings = self.db_manager.get_settings()
 
-    def start(self):
-        # Perform any necessary initialization before the bot starts running
-        self.running = True
-        self.main_loop()
+        # Initialize Data Provider Interface, Strategy Manager, and Trade Management
+        self.data_provider = DataProviderInterface(self.settings)
+        self.strategy_manager = StrategyManager(self.settings, self.db_manager)
+        self.trade_management = TradeManager(self.settings, self.db_manager)
 
-    def stop(self):
-        # Perform any necessary cleanup before the bot stops running
-        self.running = False
+        # Flag to control the trading loop
+        self.is_trading = False
 
-    def main_loop(self):
-        # The main loop where the bot's operations are orchestrated
-        while self.running:
-            # Get the latest market data
-            market_data = self.data_provider_interface.get_market_data()
+    def start_trading(self):
+        # Load the trading strategy
+        strategy = self.strategy_manager.load_strategy()
 
-            # Use the strategy manager to decide what actions to take based on the market data
-            trading_signals = self.strategy_manager.generate_trading_signals(market_data)
+        # Start the data provider
+        self.data_provider.start()
 
-            # Use the trade manager to decide when to place/cancel orders based on the trading signals
-            trade_instructions = self.trade_manager.generate_trade_instructions(trading_signals)
+        # Begin the trading loop
+        self.is_trading = True
+        while self.is_trading:
+            try:
+                # Get the latest market data
+                market_data = self.data_provider.get_data()
 
-            # Use the order execution engine to execute the trade instructions
-            self.order_execution_engine.execute_trade_instructions(trade_instructions)
+                # Execute the trading strategy
+                signals = strategy.execute(market_data)
 
-            # Here you might also want to add some sleep to control how often the loop runs
-            # time.sleep(some_interval)
+                # Process the signals
+                self.trade_management.process_signals(signals)
+
+                # Sleep for a while before next iteration
+                time.sleep(self.settings['trading_interval'])
+            except Exception as e:
+                print(f"An error occurred while trading: {e}")
+                self.stop_trading()
+
+    def stop_trading(self):
+        self.is_trading = False
+        self.data_provider.stop()
+        print("Trading has been stopped.")
+
+    def update_settings(self, new_settings):
+        self.settings = new_settings
+        self.db_manager.save_settings(new_settings)
